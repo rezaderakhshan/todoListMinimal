d7d66fc5b9b38392e6359a2af2cea0f49f8ddf73
Reza Derakhshan <r.derakhshan@tosantechno.com>
2025-12-10 12:16:47 +0330

    feat: add task

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import TaskControls from "./components/TaskControls";
 import TaskForm from "./components/TaskForm";
-import TaskList from "./components/TaskList";
+import TaskList, { type Task } from "./components/TaskList";
 
 const App = () => {
   const [tasks, setTasks] = useState([
@@ -10,6 +10,10 @@ const App = () => {
     { id: 3, text: "Read a book", priority: 3, done: false },
   ]);
   const [showOnlyIncomplete, setShowOnlyIncomplete] = useState(false);
+  const addTask = (newTask: Task) => {
+    const updatedTasks = [...tasks, newTask];
+    setTasks(updatedTasks);
+  };
   const sortTasks = () => {
     const sortedTasks = [...tasks].sort((a, b) => a.priority - b.priority);
     setTasks(sortedTasks);
@@ -24,7 +28,7 @@ const App = () => {
       }}
     >
       <h2 style={{ textAlign: "center" }}>To-Do List</h2>
-      <TaskForm />
+      <TaskForm addTask={addTask} />
       <TaskControls
         showOnlyIncomplete={showOnlyIncomplete}
         setShowOnlyIncomplete={setShowOnlyIncomplete}
--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,5 +1,64 @@
-const TaskForm = () => {
-  return <div>Task Form.</div>;
+import { Plus } from "lucide-react";
+import { useState } from "react";
+import type { Task } from "./TaskList";
+type TaskFormProps = {
+  addTask: (newTask: Task) => void;
+};
+const TaskForm = ({ addTask }: TaskFormProps) => {
+  const [newTask, setNewTask] = useState("");
+  const [newPriority, setNewPriority] = useState(1);
+  const handleSubmit = () => {
+    if (newTask.trim()) {
+      addTask({
+        id: Date.now(),
+        text: newTask,
+        priority: newPriority,
+        done: false,
+      });
+      setNewTask("");
+      setNewPriority(1);
+    }
+  };
+  const inputStyle = {
+    padding: "8px",
+    borderRadius: "5px",
+    border: "1px solid #ccc",
+  };
+  return (
+    <div>
+      <div style={{ display: "flex", gap: "10px" }}>
+        <input
+          type="text"
+          placeholder="Enter new task"
+          style={{ ...inputStyle, flexGrow: 1 }}
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <input
+          type="number"
+          min={"1"}
+          style={{ ...inputStyle, width: "3.75rem" }}
+          value={newPriority}
+          onChange={(e) => setNewPriority(+e.target.value)}
+        />
+      </div>
+      <button
+        onClick={handleSubmit}
+        style={{
+          display: "block",
+          margin: "20px auto 0",
+          borderRadius: "50%",
+          backgroundColor: "#007bff",
+          color: "#fff",
+          padding: "12px",
+          border: "none",
+          cursor: "pointer",
+        }}
+      >
+        <Plus size={20} />
+      </button>
+    </div>
+  );
 };
 
 export default TaskForm;
